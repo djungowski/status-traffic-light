b9b420b23abeb952cf3dc95d034736380c6eb303
Dominik Jungowski <Dominik@Jungowski.de>
2015-05-13 13:51:58 +0200

    Rename host to base_url

--- traffic.py
+++ traffic.py
@@ -11,13 +11,13 @@ config.read("config.ini")
 
 # Use custom requester which ignores self-signed certificates
 requester = Requester(config["jenkins"]["username"], config["jenkins"]["password"], ssl_verify=False)
-jenkins = Jenkins(config["jenkins"]["host"], requester=requester)
+jenkins = Jenkins(config["jenkins"]["base_url"], requester=requester)
 
 
 # Monkeypatch the jenkinsapi provided view
 ViewMonkeypatch().apply()
 
-view_url = config["jenkins"]["host"] + "/view/" + config["jenkins"]["view"]
+view_url = config["jenkins"]["base_url"] + "/view/" + config["jenkins"]["view"]
 view = jenkins.get_view_by_url(view_url)
 jobs = view.get_jobs()
 status = Status(jobs)
